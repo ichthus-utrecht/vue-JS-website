23fa75ca5a46db0ee9504e00e28c6b16c03b154c
André Vis <52654095+neutron-cracker@users.noreply.github.com>
2024-02-11 00:43:26 +0100

    Use existing styles

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/scss/app.scss'
+import './assets/scss/frontend.scss'
 
 createApp(App).use(router).mount('#app')

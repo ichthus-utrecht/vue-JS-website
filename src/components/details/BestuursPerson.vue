d495ed77cdc23e183935386acba7e81bb479e975
André Vis <52654095+neutron-cracker@users.noreply.github.com>
2024-02-11 00:45:48 +0100

    Add bestuursinfo view

---
+++ src/components/details/BestuursPerson.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+// eslint-disable-next-line
+defineProps({
+    imageNumber: String,
+    name: String,
+    functie: String,
+    email: String,
+})
+
+</script>
+
+
+<template>
+<div class="person-section col-12 col-md-5">
+    <div class="row align-items-center justify-content-center">
+        <div class="col-6 col-md-5">
+            <img class="section-image"  :src="require(`@/assets/images/bestuur/bestuur-person-${imageNumber}.jpg`)" alt="'"/>
+        </div>
+        <div class="col-12 col-md-7">
+            <div class="section-header">{{ name }}</div>
+            <div class="section-caption">{{ functie }}</div>
+            <div class="section-contact">{{ email }}</div>
+        </div>
+        <div class="col-12 row">
+            <div class="col-4"></div>
+            <div class="col-4">
+                <hr class="divider divider-thin divider-large"/>
+            </div>
+            <div class="col-4"></div>
+        </div>
+        <div class="col-12 section-text">
+            <slot></slot>
+        </div>
+    </div>
+</div>
+</template>
---
+++ src/views/vereniging/BestuursInfo.vue
@@ -0,0 +1,36 @@
+<script setup>
+import HeaderBalk from '../../components/interactief/HeaderBalk.vue'
+import BestuursPerson from '../../components/details/BestuursPerson.vue';
+import StandardLayoutWithHeader from '../../components/layouts/StandardLayoutWithHeader.vue'
+</script>
+
+<template>
+    <HeaderBalk />
+    <StandardLayoutWithHeader pageHeader="Bestuur der C.S.V. Ichthus Utrecht" description="Zoals elke studentenvereniging heeft ook Ichthus Utrecht een bestuur! Onderstaand stellen zij zich aan jou voor.">
+        <div class="row justify-content-center">
+            <BestuursPerson imageNumber="1" name="Mirte van den Berg" function="Praeses" email="praeses@ichthusutrecht.nl">
+                <p>Mijn naam is Mirte en dit jaar ben ik de Praeses van Ichthus Utrecht. Vanuit deze rol mag ik de vereniging en het bestuur leiden op de ALV’s en bv’s. Ook zal ik vooruit kijken naar wat er gaat komen en daarbij ons beleid in de gaten houden.</p>
+                <p>Bij mijn studie voor docent Nederlandse Gebarentaal, combineer ik mijn enthousiasme om mensen nieuwe dingen te leren en mijn liefde voor de Nederlandse Gebarentaal. In mijn vrije tijd kan ik genieten van een goed boek, muziek en creativiteit. Zo leer ik mezelf naaien, haken en maak linosnedes. Waar ik energie van krijg? Boulderen en een goed kopje koffie. Dus klop gerust aan voor een van de twee of de combinatie!</p>
+            </BestuursPerson>
+            <BestuursPerson imageNumber="2" name="Joëlle Meijer" function="Ab Actis" email="abactis@ichthusutrecht.nl">
+                <p>Mijn naam is Mirte en dit jaar ben ik de Praeses van Ichthus Utrecht. Vanuit deze rol mag ik de vereniging en het bestuur leiden op de ALV’s en bv’s. Ook zal ik vooruit kijken naar wat er gaat komen en daarbij ons beleid in de gaten houden.</p>
+                <p>Bij mijn studie voor docent Nederlandse Gebarentaal, combineer ik mijn enthousiasme om mensen nieuwe dingen te leren en mijn liefde voor de Nederlandse Gebarentaal. In mijn vrije tijd kan ik genieten van een goed boek, muziek en creativiteit. Zo leer ik mezelf naaien, haken en maak linosnedes. Waar ik energie van krijg? Boulderen en een goed kopje koffie. Dus klop gerust aan voor een van de twee of de combinatie!</p>
+            </BestuursPerson>
+            <BestuursPerson imageNumber="3" name="Nathan Bouwman" function="Fiscus" email="fiscus@ichthusutrecht.nl">
+                Hey! Ik ben Nathan en dit jaar ben ik de Fiscus van Ichthus Utrecht, jaja de man met het grote geld. Dit jaar hou ik de regie en het overzicht over de financiën van Ichthus. Naast mijn bestuurstaken leg ik dit jaar de laatste hand aan mijn bachelor Biologie aan de UU. Om van al die drukte bij te komen ben ik ook onze plaatselijke kampvuurgitarist en ben ik een grote puzzelaar.
+            </BestuursPerson>
+            <BestuursPerson imageNumber="4" name="Mirte van den Berg" function="Praeses" email="praeses@ichthusutrecht.nl">
+                <p>Mijn naam is Mirte en dit jaar ben ik de Praeses van Ichthus Utrecht. Vanuit deze rol mag ik de vereniging en het bestuur leiden op de ALV’s en bv’s. Ook zal ik vooruit kijken naar wat er gaat komen en daarbij ons beleid in de gaten houden.</p>
+                <p>Bij mijn studie voor docent Nederlandse Gebarentaal, combineer ik mijn enthousiasme om mensen nieuwe dingen te leren en mijn liefde voor de Nederlandse Gebarentaal. In mijn vrije tijd kan ik genieten van een goed boek, muziek en creativiteit. Zo leer ik mezelf naaien, haken en maak linosnedes. Waar ik energie van krijg? Boulderen en een goed kopje koffie. Dus klop gerust aan voor een van de twee of de combinatie!</p>
+            </BestuursPerson>
+            <BestuursPerson imageNumber="5" name="Mirte van den Berg" function="Praeses" email="praeses@ichthusutrecht.nl">
+                <p>Mijn naam is Mirte en dit jaar ben ik de Praeses van Ichthus Utrecht. Vanuit deze rol mag ik de vereniging en het bestuur leiden op de ALV’s en bv’s. Ook zal ik vooruit kijken naar wat er gaat komen en daarbij ons beleid in de gaten houden.</p>
+                <p>Bij mijn studie voor docent Nederlandse Gebarentaal, combineer ik mijn enthousiasme om mensen nieuwe dingen te leren en mijn liefde voor de Nederlandse Gebarentaal. In mijn vrije tijd kan ik genieten van een goed boek, muziek en creativiteit. Zo leer ik mezelf naaien, haken en maak linosnedes. Waar ik energie van krijg? Boulderen en een goed kopje koffie. Dus klop gerust aan voor een van de twee of de combinatie!</p>
+            </BestuursPerson>
+            <BestuursPerson imageNumber="6" name="Mirte van den Berg" function="Praeses" email="praeses@ichthusutrecht.nl">
+                <p>Mijn naam is Mirte en dit jaar ben ik de Praeses van Ichthus Utrecht. Vanuit deze rol mag ik de vereniging en het bestuur leiden op de ALV’s en bv’s. Ook zal ik vooruit kijken naar wat er gaat komen en daarbij ons beleid in de gaten houden.</p>
+                <p>Bij mijn studie voor docent Nederlandse Gebarentaal, combineer ik mijn enthousiasme om mensen nieuwe dingen te leren en mijn liefde voor de Nederlandse Gebarentaal. In mijn vrije tijd kan ik genieten van een goed boek, muziek en creativiteit. Zo leer ik mezelf naaien, haken en maak linosnedes. Waar ik energie van krijg? Boulderen en een goed kopje koffie. Dus klop gerust aan voor een van de twee of de combinatie!</p>
+            </BestuursPerson>
+        </div>
+    </StandardLayoutWithHeader>
+</template>

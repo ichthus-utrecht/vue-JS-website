ed5e359935e57da299ec992307c0b72717ec9e31
Rogue-Juan <91721131+Rogue-Juan@users.noreply.github.com>
2024-02-11 23:05:29 +0100

    Copied from old website

--- src/views/activiteiten/GeloofPagina.vue
+++ src/views/activiteiten/GeloofPagina.vue
@@ -7,8 +7,64 @@ import StandardLayoutWithHeader from '../../components/layouts/StandardLayoutWit
     <HeaderBalk />
     <StandardLayoutWithHeader pageHeader="Activiteiten in het geloof"
         description="Omdat het christendom centraal staat op onze vereniging, doen wij ook samen veel activiteiten met betrekking tot het geloof. Ichthus is een plek om te groeien in het geloof en samen nieuwe ervaringen mee te maken.">
-        <div class="row justify-content-center">
-
+        <div class="justify-content-center activity-section-wrapper">
+            <div class="row activity-section mt-4">
+                <div class="col-12 col-md-4">
+                    <div class="post-polaroid">
+                        <img class="post-polaroid-image polaroid-image-l" src="@/assets/images/activiteit-1.jpg" alt="'">
+                        <div class="post-polaroid-caption polaroid-caption-l">Hechte groep!</div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-8 section-description">
+                    <div class="row section-header">Kringen</div>
+                    <div class="row post-text">De kringen zijn de kern van de vereniging. Iedereen die lid is van Ichthus,
+                        wordt elk jaar op een kring geplaatst. Een kring bestaat uit 6 tot 8 personen en komt wekelijks op
+                        dinsdagavond bij elkaar om samen te eten, te bidden, te zingen en een Bijbelstudie te doen. De
+                        Bijbelstudie wordt steeds geleid door een of twee kringleden. Het doel van de kring is dat leden
+                        opgebouwd worden in hun geloof, dat leden elkaar bemoedigen en elkaar helpen om te groeien in hun
+                        relatie met God. Naast bijbelstudies houden kringen ook af en toe een zogenaamde 'kliek': dit is een
+                        avond, waarop de kring bijvoorbeeld uit eten gaat, een film kijkt met elkaar of een maatschappelijke
+                        actie doet.</div>
+                </div>
+            </div>
+            <div class="row activity-section mt-4">
+                <div class="col-12 col-md-8 section-description">
+                    <div class="section-header">Verdiepende activiteiten</div>
+                    <div class="post-text">Een aantal keer per jaar vindt er na de maaltijd of op een verenigingsweekend een
+                        Praise & Prayer (P&P) plaats. Dit zijn diensten, georganiseerd door de P&P-commissie en begeleid
+                        door onze band, waarin we samen onze focus bewust op God richten. We aanbidden God, we bidden samen
+                        en we luisteren naar sprekers, getuigenissen en filmpjes. Hierdoor worden we bemoedigd en aan het
+                        denken gezet. Daarnaast hebben we ook Missie-activiteiten, die gericht zijn op toerusting en
+                        evangelisatie in de praktijk brengen. De afgelopen jaren zijn er heel wat verschillende acties
+                        geweest, zoals afwassen bij studentenhuizen, kerstkoekjes uitdelen en zingen op het station. Ook
+                        zijn we actief op de Uithof. Er is de mogelijkheid om maandelijks in tweetallen geloofsgesprekken
+                        met studenten te voeren. Het onderwerp van gesprek verschilt per keer, maar we proberen aan zoveel
+                        mogelijk mensen het goede nieuws over Jezus te vertellen. </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="post-polaroid">
+                        <img class="post-polaroid-image polaroid-image-l" src="@/assets/images/activiteit-2.jpg" alt="'">
+                        <div class="post-polaroid-caption polaroid-caption-l">Missie Avond 10/11</div>
+                    </div>
+                </div>
+            </div>
+            <div class="row activity-section mt-4">
+                <div class="col-12 col-md-4">
+                    <div class="post-polaroid">
+                        <img class="post-polaroid-image polaroid-image-l" src="@/assets/images/activiteit-3.jpg" alt="'">
+                        <div class="post-polaroid-caption polaroid-caption-l">Samenkomen</div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-8 section-description">
+                    <div class="section-header">Gebed</div>
+                    <div class="post-text">Bij Ichthus speelt gebed een belangrijke rol, omdat wij geloven dat gebed
+                        essentieel is voor het geloofsleven. Het M-team brengt de vereniging in gebed bij God en stimuleert
+                        gebed bij Ichthianen. Daarnaast zijn ze beschikbaar tijdens P&P’s en andere activiteiten om met
+                        leden te bidden en stimuleren ze het vormen van gebedsgroepjes. Een gebedsgroepje bestaat uit 2 of 3
+                        Ichthianen die op regelmatige basis bij elkaar komen om te bidden. Verder hebben ze één keer in het
+                        jaar een donderdagavond beschikbaar die in het teken staat van gebed.</div>
+                </div>
+            </div>
         </div>
     </StandardLayoutWithHeader>
 </template>

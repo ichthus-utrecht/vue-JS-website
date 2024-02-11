d12e2f718b58634db0d8b7db925c612878821457
Rogue-Juan <91721131+Rogue-Juan@users.noreply.github.com>
2024-02-11 22:49:59 +0100

    Initial sketch

--- src/views/activiteiten/GeloofPagina.vue
+++ src/views/activiteiten/GeloofPagina.vue
@@ -4,4 +4,10 @@ import HeaderBalk from '../../components/interactief/HeaderBalk.vue'
 
 <template>
     <HeaderBalk />
+    <StandardLayoutWithHeader pageHeader="Activiteiten in het geloof"
+        description="Omdat het christendom centraal staat op onze vereniging, doen wij ook samen veel activiteiten met betrekking tot het geloof. Ichthus is een plek om te groeien in het geloof en samen nieuwe ervaringen mee te maken.">
+        <div class="row justify-content-center">
+
+        </div>
+    </StandardLayoutWithHeader>
 </template>

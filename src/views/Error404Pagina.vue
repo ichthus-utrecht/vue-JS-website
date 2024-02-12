5fcb194d89b93ab889ffd8df5b2477d36f1aae32
607 <607@607web.nl>
2024-02-12 17:13:46 +0100

    feat: Error404Pagina toegevoegd en aan router toegevoegd als catch-all

---
+++ src/views/Error404Pagina.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HeaderBalk from '../components/interactief/HeaderBalk.vue'
+import StandardLayoutWithHeader from '../components/layouts/StandardLayoutWithHeader.vue'
+</script>
+
+<template>
+    <HeaderBalk />
+    <StandardLayoutWithHeader pageHeader="Error 404 – Pagina niet gevonden"
+        description="De pagina die je op hebt gevraagd bestaat niet. Mogelijk klopt het adres niet.">
+
+    </StandardLayoutWithHeader>
+</template>

087eb0e35205d2ea5d794b2ffb76e820f6290cad
Rogue-Juan <91721131+Rogue-Juan@users.noreply.github.com>
2024-02-13 00:56:14 +0100

    lock added

--- src/components/interactief/HeaderBalk.vue
+++ src/components/interactief/HeaderBalk.vue
@@ -64,7 +64,11 @@ import { ref } from 'vue'
         </div>
         <div class="menu-item">
             <a class="nav-link" href="https://ichthusutrecht.churchbook.nl">
-                <i class="fas fa-lock fa-xs m-2" id="headerLock" style="color: gray;"></i>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon" aria-hidden="true" focusable="false"
+                    viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                    <path fill="gray"
+                        d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z" />
+                </svg>
                 <NavigatieKnop tekst="Leden" koppeling="https://ichthusutrecht.churchbook.nl" />
             </a>
         </div>
@@ -85,6 +89,12 @@ import { ref } from 'vue'
     height: 60px;
     margin-left: 10%;
     margin-right: 25%;
+}
+
+.icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -.125em;
 }
 
 .menu-item {

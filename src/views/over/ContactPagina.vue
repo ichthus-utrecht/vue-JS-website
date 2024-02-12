1d073812f779372db5321efb8e73c438871a53c6
Rogue-Juan <91721131+Rogue-Juan@users.noreply.github.com>
2024-02-12 20:39:03 +0100

    Update ContactPagina.vue

--- src/views/over/ContactPagina.vue
+++ src/views/over/ContactPagina.vue
@@ -1,10 +1,230 @@
 <script setup>
 import HeaderBalk from '../../components/interactief/HeaderBalk.vue'
-
+import StandardLayoutWithHeader from '../../components/layouts/StandardLayoutWithHeader.vue'
 </script>
 
 <template>
     <HeaderBalk />
+    <StandardLayoutWithHeader pageHeader="Contact" description=" Bij vragen of opmerkingen richting de besturende kern van Ichthus, of vraag je je af hoe je lid moet worden of is er
+        iets anders wat nog niet duidelijk is? Kijk even of je vraag in de FAQ-lijst staat.
+        Kun je je vraag niet vinden? Je kunt altijd contact met ons opnemen met het formulier onderaan de pagina.">
+        <div class="row justify-content-center">
+            <div class="col-12 page-section-header-m mt-0">
+                FAQ - De meest gestelde vragen
+                <hr class="divider divider-thin divider-gray" />
+            </div>
+            <div class="faq-container col-12">
+                <div class="row" id="accordion">
+                    <div class="card col-12 mt-3">
+                        <h4 class="card-header faq-question">
+                            <a class="accordion-toggle text-red" data-bs-toggle="collapse" data-parent="#accordion"
+                                href="#collapseOne">Kan ik met een student van jullie meelopen op zijn of haar studie?</a>
+                        </h4>
+                        <div id="collapseOne" class="panel-collapse collapse in">
+                            <div class="card-block faq-answer">
+                                De studierichtingen bij Ichthus zijn zeer divers: grote kans dus ook dat er op de vereniging
+                                iemand
+                                rondloopt met de studie van jouw interesse waar je een dagje mee kan lopen.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card col-12 mt-3">
+                        <h4 class="card-header faq-question">
+                            <a class="accordion-toggle text-red" data-bs-toggle="collapse" data-parent="#accordion"
+                                href="#collapseTwo">Kan ik aan een kamer komen via Ichthus?</a>
+                        </h4>
+                        <div id="collapseTwo" class="panel-collapse collapse in">
+                            <div class="card-block faq-answer">
+                                Ichthus heeft geen eigen huizen in bezit, maar als je eenmaal lid wordt kun je vaak wel via
+                                via
+                                aan
+                                een kamer komen in een huis met Ichthianen.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card col-12 mt-3">
+                        <h4 class="card-header faq-question">
+                            <a class="accordion-toggle text-red" data-bs-toggle="collapse" data-parent="#accordion"
+                                href="#collapseThree">Aan wat voor eisen moet ik voldoen om lid te mogen worden van de
+                                vereniging?</a>
+                        </h4>
+                        <div id="collapseThree" class="panel-collapse collapse in">
+                            <div class="card-block faq-answer">
+                                Je moet ingeschreven zijn bij een HBO- of WO-opleiding. Je hoeft hiervoor niet per se in
+                                Utrecht
+                                te
+                                studeren. Daarnaast verwachten we ook dat je een christelijke levensovertuiging hebt als je
+                                lid
+                                wordt van de vereniging.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card col-12 mt-3">
+                        <h4 class="card-header faq-question">
+                            <a class="accordion-toggle text-red" data-bs-toggle="collapse" data-parent="#accordion"
+                                href="#collapseFour">Mag ik als MBO-student lid zijn van Ichthus?</a>
+                        </h4>
+                        <div id="collapseFour" class="panel-collapse collapse in">
+                            <div class="card-block faq-answer">
+                                In onze statuten staat vastgelegd dat MBO-studenten geen lid mogen worden van de vereniging.
+                                Dit
+                                komt mede doordat vanuit de Hogeschool Utrecht (HU) en de Universiteit Utrecht (UU) de eis
+                                staat
+                                dat
+                                70% van de vereniging bestaat uit studenten aan de HU of UU. Om erkend te worden als
+                                studentenvereniging moeten we voldoen aan deze eis.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card col-12 mt-3">
+                        <h4 class="card-header faq-question">
+                            <a class="accordion-toggle text-red" data-bs-toggle="collapse" data-parent="#accordion"
+                                href="#collapseFive">Is het volgen van de introductieweek verplicht?</a>
+                        </h4>
+                        <div id="collapseFive" class="panel-collapse collapse in">
+                            <div class="card-block faq-answer">
+                                Het volgen van de introductieweek in september is niet verplicht om lid te worden van de
+                                vereniging.
+                                Wel wordt het van harte aanbevolen om deel te nemen, omdat het een goede manier is om kennis
+                                te
+                                maken met de vereniging en andere eerstejaars. Daarnaast is er paar weken later een
+                                startweekend,
+                                waar je vervolgens wordt geïnstalleerd als lid. Het wordt ook van harte aanbevolen om
+                                hierbij
+                                aanwezig te zijn als je lid wilt worden.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card col-12 mt-3">
+                        <h4 class="card-header faq-question">
+                            <a class="accordion-toggle text-red" data-bs-toggle="collapse" data-parent="#accordion"
+                                href="#collapseSix">Word ik ontgroend?</a>
+                        </h4>
+                        <div id="collapseSix" class="panel-collapse collapse in">
+                            <div class="card-block faq-answer">
+                                Nee, Ichthus Utrecht doet niet aan een ontgroening voor de eerstejaars. In september vindt
+                                een
+                                introductieweek plaats en in oktober/november een eerstejaarsweekend, maar ontgroening is
+                                daar
+                                geen
+                                deel van.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card col-12 mt-3">
+                        <h4 class="card-header faq-question">
+                            <a class="accordion-toggle text-red" data-bs-toggle="collapse" data-parent="#accordion"
+                                href="#collapseSeven">Wat zijn de verplichtingen van Ichthus?</a>
+                        </h4>
+                        <div id="collapseSeven" class="panel-collapse collapse in">
+                            <div class="card-block faq-answer">
+                                Van elk lid wordt verwacht dat hij of zij deelneemt aan de Bijbelkring. Dit is eenmaal per
+                                week
+                                op
+                                de dinsdag (en bij uitzondering ook op maandag). Daarnaast heb je een paar keer per jaar een
+                                maaltijdtaak tijdens de algemene avonden en ben je verplicht om aanwezig te zijn bij de
+                                Algemene
+                                Ledenvergaderingen (in de regel tussen de drie en vijf keer per jaar). Het is gebruikelijk
+                                dat je
+                                ook je steentje bijdraagt – in zoverre dat voor jou mogelijk is – door een commissierol te
+                                vervullen.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card col-12 mt-3">
+                        <h4 class="card-header faq-question">
+                            <a class="accordion-toggle text-red" data-bs-toggle="collapse" data-parent="#accordion"
+                                href="#collapseEight">Kan ik bij Ichthus als ik een beperking heb?</a>
+                        </h4>
+                        <div id="collapseEight" class="panel-collapse collapse in">
+                            <div class="card-block faq-answer">
+                                Ja dat kan, we willen voor iedereen toegankelijk zijn als vereniging en we doen hier ook ons
+                                best
+                                voor om jou ook een thuis te bieden bij Ichthus. Samen met jou willen we kijken wat de
+                                mogelijkheden
+                                zijn.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card col-12 mt-3">
+                        <h4 class="card-header faq-question">
+                            <a class="accordion-toggle text-red" data-bs-toggle="collapse" data-parent="#accordion"
+                                href="#collapseNine">Is het belangrijk uit welke kerkelijke stroming je komt?</a>
+                        </h4>
+                        <div id="collapseNine" class="panel-collapse collapse in">
+                            <div class="card-block faq-answer">
+                                Nee, als Ichthus zijn we een interkerkelijke vereniging. De diversiteit in kerkelijke
+                                achtergronden
+                                zorgt ervoor dat je door met elkaar in gesprek te gaan je eigen horizon kunt verbreden en
+                                veel
+                                van
+                                elkaar kunt leren.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card col-12 mt-3">
+                        <h4 class="card-header faq-question">
+                            <a class="accordion-toggle text-red" data-bs-toggle="collapse" data-parent="#accordion"
+                                href="#collapseTen">Kan ik later in het jaar lid worden van de vereniging?</a>
+                        </h4>
+                        <div id="collapseTen" class="panel-collapse collapse in">
+                            <div class="card-block faq-answer">
+                                Later lid worden van de vereniging is in sommige gevallen mogelijk. Dit gebeurt altijd in
+                                overleg
+                                met jou en het bestuur.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 page-section-header-m mt-0">
+                Contactgegevens
+                <hr class="divider divider-thin divider-gray" />
+            </div>
+            <div class="col-12 col-md-6">
+                <p class="post-text">Ichthus Utrecht is te benaderen via de onderstaande gegevens.</p>
+
+                <strong>Postadres</strong>
+                <p>
+                    C.S.V. Ichthus Utrecht, Pnyx<br>
+                    Winthontlaan 30C<br>
+                    3526KV Utrecht<br>
+                </p>
+
+                <strong>Locatie</strong>
+                <p>
+                    Boorstraat 107<br>
+                    3513 SE Utrecht<br>
+                    abactis@ichthusutrecht.nl
+                </p>
+            </div>
+            <div class="col-12 col-md-6">
+                <h1 class="page-section-header-s post-text pb-0">Contactpersoon</h1>
+                <div class="person-card">
+                    <div class="row w-100 person-section">
+                        <div class="col-6">
+                            <img class="section-image" src="@/assets/images/bestuur/bestuur-person-2.jpg" alt="'" />
+                        </div>
+                        <div class="col-6">
+                            <div class=" section-header">Joëlle Meijer</div>
+                            <div class="section-contact">
+                                abactis@ichthusutrecht.nl<br>
+                                +31 6 - 425 720 29
+                            </div>
+                            <div class="section-social">
+                                <!-- <a href="https://www.linkedin.com/in/sophie-van-dijk-b361ab174" target="_blank" class="btn-social btn-sm btn-linkedin"><i class="fa fa-linkedin"></i></a> -->
+                                <a href="https://nl-nl.facebook.com/csvichthusutrecht" target="_blank"
+                                    class="btn-social btn-sm btn-facebook"><i class="fa fa-facebook"></i></a>
+                                <a href="https://www.instagram.com/ichthus_utrecht/" target="_blank"
+                                    class="btn-social btn-sm btn-instagram"><i class="fa fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </StandardLayoutWithHeader>
 </template>
 
 <style lang="css">

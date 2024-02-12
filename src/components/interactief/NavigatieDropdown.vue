3c6d9e4f5ef9a6ec410260e4cabd74f07bda3369
Rogue-Juan <91721131+Rogue-Juan@users.noreply.github.com>
2024-02-12 01:20:19 +0100

    Aesthetic updates to HeaderBalk

--- src/components/interactief/NavigatieDropdown.vue
+++ src/components/interactief/NavigatieDropdown.vue
@@ -24,28 +24,45 @@ function inklappen() {
 
 <template>
     <div class="dropdown" @mouseover="uitklappen" @mouseleave="inklappen">
-        <a class="uitklapknop" href="#" >{{ tekst }}</a>
-        <ul class="dropdowninhoud" v-if="uitgeklapt"> <!-- De inhoud van de dropdown wordt alleen weergegeven als hij uitgeklapt is -->
-            <li v-for="knop in inhoud.value" :key="knop.tekst"> <!-- We gebruiken een lijst om de knoppen onder elkaar weer te geven -->
-                <NavigatieKnop class="inhoudsknop" :tekst="knop.tekst" :koppeling="knop.koppeling"/>
+        <a class="uitklapknop" href="#">{{ tekst }}</a>
+        <ul class="dropdowninhoud" v-if="uitgeklapt">
+            <!-- De inhoud van de dropdown wordt alleen weergegeven als hij uitgeklapt is -->
+            <li v-for="knop in inhoud.value" :key="knop.tekst">
+                <!-- We gebruiken een lijst om de knoppen onder elkaar weer te geven -->
+                <NavigatieKnop class="inhoudsknop" :tekst="knop.tekst" :koppeling="knop.koppeling" />
             </li>
         </ul>
     </div>
-
-
 </template>
 
 
 <style scoped>
-
 .dropdowninhoud {
-    position: absolute; /* Zorg ervoor dat de inhoud onder de knop terechtkomt en de knop niet van plek veranderd */
-    list-style-type: none; /* Geen bolletjes bij de opties */
+    position: absolute;
+    /* Zorg ervoor dat de inhoud onder de knop terechtkomt en de knop niet van plek veranderd */
+    list-style-type: none;
+    /* Geen bolletjes bij de opties */
     /* padding gebruiken ipv margin zodat mouseleave niet triggert bij de witruimte */
-    padding: 0px;
-    padding-top: 20px;
+    padding: 30px;
+    padding-top: 60px;
+    padding-bottom: 10px;
+    background-color: white;
     margin: 0px;
     text-align: left;
+    white-space: nowrap;
+}
+
+.dropdowninhoud::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 58px;
+    height: calc(100% - 58px);
+    /* Adjust the height of the line as needed */
+    width: 2px;
+    /* Adjust the width of the line as needed */
+    background-color: rgb(151, 0, 70);
+    transition: width 0.3s;
 }
 
 /* Dit is momenteel gekopieerd uit NavigatieKnop.vue; als deze af is kijken of we het beide in een component kunnen zetten die hergebruikt wordt, of misschien stylen in een ander component */

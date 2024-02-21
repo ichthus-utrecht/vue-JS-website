c57437fff9fa843cc75cfde548ccceb276ab00b1
Rogue-Juan <91721131+Rogue-Juan@users.noreply.github.com>
2024-02-21 16:35:22 +0100

    Small updates
    Title "Ichthus Utrecht" links to home page; code made cleaner

--- src/components/interactief/HeaderBalk.vue
+++ src/components/interactief/HeaderBalk.vue
@@ -12,7 +12,9 @@ import { ref } from 'vue'
             <router-link to="/"><img src="../../assets/images/icons/ichthus-logo-red.png" class="logo"></router-link>
         </div>
         <div class="menu-item">
-            <div class="titleCorner">Ichthus Utrecht</div>
+            <router-link to="/" style="text-decoration: none;">
+                <div class="titleCorner">Ichthus Utrecht</div>
+            </router-link>
         </div>
         <div class="menu-item">
             <NavigatieDropdown tekst="Vereniging" :inhoud="ref([
@@ -65,7 +67,7 @@ import { ref } from 'vue'
         <div class="menu-item">
             <a class="nav-link" href="https://ichthusutrecht.churchbook.nl">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" aria-hidden="true" focusable="false"
-                    viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                    viewBox="0 0 448 512"><!-- !Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
                     <path fill="gray"
                         d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z" />
                 </svg>

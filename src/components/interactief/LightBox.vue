5c01c201e90d1939658cf2d912a5e06d7aa5283e
607 <607@607web.nl>
2024-06-10 12:31:08 +0200

    feat: X toevoegen als kruisje

--- src/components/interactief/LightBox.vue
+++ src/components/interactief/LightBox.vue
@@ -10,7 +10,7 @@
 
 <template>
     <div class="lightbox" id="lightbox-{{ $selector }}" v-if="tonen" style="-moz-user-select: none; -webkit-user-select: none; -ms-user-select:none; user-select:none;-o-user-select:none; background-color: rgba(0,0,40,0.8)" unselectable="on" onselectstart="return false;" onmousedown="return false;">
-        <i class="fas fa-times fa-3x lightbox-cross" @click="verdwijnen"></i>
+        <b class="fas fa-3x lightbox-cross" @click="verdwijnen">X</b>
 
     <div class="lightbox-modal">
         <slot></slot>

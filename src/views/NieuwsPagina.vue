ada9f542cdcba367cf8a161afbddc501860dd137
André Vis <52654095+neutron-cracker@users.noreply.github.com>
2024-08-16 15:45:26 +0200

    Maak nieuws pagina met een nummer

---
+++ src/views/NieuwsPagina.vue
@@ -0,0 +1,29 @@
+<script setup>
+import BasisLayout from '../components/layouts/BasisLayout.vue'
+import FooterBalk from '../components/interactief/FooterBalk.vue'
+import NavigatieBalk from '@/components/interactief/NavigatieBalk.vue'
+import nieuwsItems from '../assets/nieuwsItems.json'
+// eslint-disable-next-line
+const props = defineProps({
+    nummer: Number
+})
+
+const item = nieuwsItems.items[props.nummer]
+const titel = item.titel
+const tekst = item.inhoud
+
+</script>
+
+<template>
+    <NavigatieBalk />
+    <BasisLayout :pageHeader="titel">
+        <div class="col-12 col-md-12">
+            {{tekst}}
+        </div>
+    </BasisLayout>
+    <FooterBalk />
+</template>
+
+<style lang="css">
+@import '../assets/css/app.css';
+</style>

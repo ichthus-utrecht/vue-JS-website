7ed8c82e7f69f4ee0b7400d5c49865e189523236
ThannerJonna <jonathane.vandijk@gmail.com>
2024-08-16 15:43:22 +0200

    Aanpassingen begin collegejaar
    UITweek toegevoegd als belangrijke gebeurtenis (data gefixt)
    Link aangepast introformulier
    Comments minder breed gemaakt

--- src/views/LidWordenPagina.vue
+++ src/views/LidWordenPagina.vue
@@ -13,7 +13,8 @@ import NavigatieBalk from '@/components/interactief/NavigatieBalk.vue';
             <div class="post-text">
                 <p>
                     Een Ichthusweek bestaat uit twee avonden. Op dinsdagavond is er kring, waarbij je samen met een vast
-                    groepje een Bijbelstudie doet. Elke donderdagavond hebben we een verenigingsavond. Die start om 18:00
+                    groepje een Bijbelstudie doet. Elke donderdagavond hebben we een verenigingsavond. Die start om
+                    18:00
                     uur in Dijckzigt (Boorstraat 107) met een maaltijd. Na de maaltijd begint de activiteit. Dat kan van
                     alles zijn. Soms is het een lezing of een debat, soms spelen we spelletjes of hebben we een Praise &
                     Prayer. In de <router-link to="/agenda">agenda</router-link> kun je zien wanneer
@@ -22,13 +23,17 @@ import NavigatieBalk from '@/components/interactief/NavigatieBalk.vue';
                 </p>
                 <!--Hier staan de stukjes voor open kringavond-->
                 <!--<p>
-                    We organiseren ook een open avond, speciaal voor mensen die overwegen lid te worden. Tijdens die avond leggen we ook wat meer uit over wie wij zijn als vereniging en waar wij voor staan.
+                    We organiseren ook een open avond, speciaal voor mensen die overwegen lid te worden. 
+                    Tijdens die avond leggen we ook wat meer uit over wie wij zijn als vereniging en waar wij voor staan.
                 </p>-->
                 <!--<p>
-                    Wil je weten hoe een kringavond eruitziet? Geef je dan op <a href="https://forms.gle/rGjgdTVnweetGKyaA">hier</a> voor de open kringavond. De deadline voor het aanmelden is zaterdag 17 juni.
+                    Wil je weten hoe een kringavond eruitziet? Geef je dan op <a href="https://forms.gle/rGjgdTVnweetGKyaA">hier</a> 
+                    voor de open kringavond. De deadline voor het aanmelden is zaterdag 17 juni.
                 </p>-->
                 <!--<p>
-                    Tijdens de UIT-week, van 14 tot 17 augustus zijn wij van de partij. Tijdens deze week organiseren we meerdere activiteiten, daar kun je ook kennismaken met Ichthus. Kom je ook langs? Vergeet je dan niet op te geven voor de UIT via <a href="https://utrechtseintroductietijd.nl/">deze link</a>.
+                    Tijdens de UIT-week, van 14 tot 17 augustus zijn wij van de partij. 
+                    Tijdens deze week organiseren we meerdere activiteiten, daar kun je ook kennismaken met Ichthus. Kom je ook langs? 
+                    Vergeet je dan niet op te geven voor de UIT via <a href="https://utrechtseintroductietijd.nl/">deze link</a>.
                 </p>    -->
             </div>
         </div>
@@ -38,12 +43,19 @@ import NavigatieBalk from '@/components/interactief/NavigatieBalk.vue';
             <div class="post-text">
                 <p>
                     Natuurlijk wil je zo snel mogelijk lid worden. Helaas kan dat pas in het nieuwe verenigingsjaar. We
-                    vragen iedereen die interesse heeft om lid te worden om mee te doen aan onze introductieweek. Dat is een
-                    week waarin we elke avond een activiteit organiseren, om elkaar en de vereniging beter te leren kennen.
-                    <!--Voor de introductieweek moet je je aanmelden in <a href="https://forms.gle/NHZwqyVsHW2uSCnE6">het formulier</a>.-->
+                    vragen iedereen die interesse heeft om lid te worden om mee te doen aan onze introductieweek. Dat is
+                    een
+                    week waarin we elke avond een activiteit organiseren, om elkaar en de vereniging beter te leren
+                    kennen.
+                    Voor de introductieweek moet je je aanmelden in <a
+                        href="https://docs.google.com/forms/d/e/1FAIpQLSd4UTW4W6Y7wSZzoWE93wlxu2mSy4bjeFJ2n2vdTySq4nmmQg/viewform?usp=sf_link">het
+                        formulier</a>.
                 </p>
                 <!--<p>
-                    <a href="https://ichthusutrecht.churchbook.nl/public/join.cfm?kerk_id=1">Het aanmeldformulier voor Ichthus Utrecht</a> is nu open! Door dit formulier in te vullen, geef je aan dat je lid wilt worden.  Het bestuur zal daarna contact met je opnemen. Je hoeft geen ontgroening te doorgaan. En als je al zeker weet dat je lid wordt, houdt dan ook vooral het startweekend vrij! Dat is de beste manier om de leden te leren kennen en je Ichthuscarrière te beginnen. 
+                    <a href="https://ichthusutrecht.churchbook.nl/public/join.cfm?kerk_id=1">Het aanmeldformulier voor Ichthus Utrecht</a> 
+                    is nu open! Door dit formulier in te vullen, geef je aan dat je lid wilt worden.  Het bestuur zal daarna contact met je opnemen. 
+                    Je hoeft geen ontgroening te doorgaan. En als je al zeker weet dat je lid wordt, houdt dan ook vooral het startweekend vrij! 
+                    Dat is de beste manier om de leden te leren kennen en je Ichthuscarrière te beginnen. 
                 </p>-->
                 <p>
                     Belangrijke data op een rijtje:
@@ -68,12 +80,12 @@ import NavigatieBalk from '@/components/interactief/NavigatieBalk.vue';
                             <td>22 juni 2023 </td>
                             <td>Open verenigingsavond. Aanmelden kan via <a href="https://forms.gle/P5yd4B4uvCyz8o587">deze link</a>.</td>
 					</tr>-->
-                    <!--<tr>
-                            <td>14–17 augustus 2023</td>
-                            <td><a href="https://utrechtseintroductietijd.nl/">UIT-week</a></td>
-					</tr>-->
                     <tr>
-                        <td>9-13 september 2024</td>
+                            <td>12–16 augustus 2023</td>
+                            <td><a href="https://utrechtseintroductietijd.nl/">UIT-week</a></td>
+					</tr>
+                    <tr>
+                        <td>9-12 september 2024</td>
                         <td><a>Introweek</a></td>
                     </tr>
                     <tr>

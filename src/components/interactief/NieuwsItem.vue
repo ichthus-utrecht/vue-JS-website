ac254d39b1d069c33ef473185e42f2b3f03dd810
607 <607@607web.nl>
2024-09-11 16:09:17 +0200

    feat: geef geen 'lees meer' weer als het hele nieuwsitem al wordt weergegeven

--- src/components/interactief/NieuwsItem.vue
+++ src/components/interactief/NieuwsItem.vue
@@ -8,12 +8,17 @@ const charactersToShow = 700
 const nummer = nieuwsItems.items.length - props.nummer // Nieuws van nieuw naar oud laten zien
 let tekst = ""
 let titel = ""
+let ingekort = false
 if (nummer >= 0) // Niet proberen meer nieuws te laten zien dan er is
 {
     const item = nieuwsItems.items[nummer]
     titel = item.titel
     tekst = item.inhoud.substring(0, charactersToShow) // Eerste n tekens
-    if (item.inhoud.length > charactersToShow) tekst += '...'
+    if (item.inhoud.length > charactersToShow) 
+    {
+        tekst += '...'
+        ingekort = true
+    }
 }
 const url = "/nieuws/" + nummer
 </script>
@@ -27,7 +32,7 @@ const url = "/nieuws/" + nummer
         <h4 class="h4 font-primary text-primary">{{ titel }}</h4>
         <p class="text-justify">
             {{tekst}}
-            <br/><br/><a :href="url">Lees meer ></a>     
+            <br/><br/><a :href="url" v-if="ingekort">Lees meer ></a>     
         </p>
      </div>
 </div>

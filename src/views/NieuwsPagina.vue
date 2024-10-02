870024b265d1c197e405c7b0a04e1dd143581524
André Vis <52654095+neutron-cracker@users.noreply.github.com>
2024-10-02 20:05:48 +0200

    fix build errors for new code

--- src/views/NieuwsPagina.vue
+++ src/views/NieuwsPagina.vue
@@ -1,12 +1,12 @@
-<script setup>
+<script setup lang="ts">
 import BasisLayout from '../components/layouts/BasisLayout.vue'
 import FooterBalk from '../components/interactief/FooterBalk.vue'
 import NavigatieBalk from '@/components/interactief/NavigatieBalk.vue'
 import nieuwsItems from '../assets/nieuwsItems.json'
-// eslint-disable-next-line
-const props = defineProps({
-  nummer: Number
-})
+
+const props = defineProps<{
+  nummer: number
+}>()
 
 const item = nieuwsItems.items[props.nummer]
 const titel = item.titel

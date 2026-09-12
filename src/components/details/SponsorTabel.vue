de04fd34241a973962472583437e246592803007
Ingrid Hekking <ingrid@gghekking.com>
2026-09-12 14:53:56 +0200

    Bug update

--- src/components/details/SponsorTabel.vue
+++ src/components/details/SponsorTabel.vue
@@ -3,7 +3,7 @@
         <div class="col-12" id="sponsorContainer">
             <div class="sponsor-frame d-flex flex-wrap justify-content-center">
                 <a href="https://www.mistersteven.nl/">
-                    <img src="@\assets\images\sponsors\Logo Mister Steven 1764x758.png">
+                    <img src="@\assets\images\sponsors\Logo De Heerenkamer 1764x758.png">
                 </a>
                 <a href="https://www.kleine-rijopleidingen.nl/">
                     <img src="@/assets/images/sponsors/kleine-rijopleidingen-maar-dan-beter.jpeg">

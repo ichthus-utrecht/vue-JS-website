937458a08ef1d392281e39eb2363a82629b6cdd2
Marc Borkhuis <marc.borkhuis@gmail.com>
2026-09-15 16:12:33 +0200

    Fix email link formatting in LHBTIQ+ page

--- src/views/over/LHBTIQ+.vue
+++ src/views/over/LHBTIQ+.vue
@@ -15,7 +15,7 @@ import NavigatieBalk from '@/components/interactief/NavigatieBalk.vue';
             </div>
             <p>
                 Wij als Ichthus Utrecht zijn een open vereniging waar voor iedereen een plek is.  In de huidige samenleving is het onderwerp LHBTIQ+ erg actueel.  Bij Ichthus is iedereen welkom ongeacht verschillen in Bijbelse visie en interpretatie op hedendaagse onderwerpen. Ook als je nog twijfelt over je geaardheid of gender, je onlangs uit de kast gekomen bent of het nog juist voor jezelf houdt. Voel je dus vooral welkom om een keer langs te komen op onze vereniging om gezellig samen te eten, te kletsen en borrelen!
-                Mocht je vragen over dit onderwerp hebben? Dan kan je abactis@ichthusutrecht.nl mailen
+                Mocht je vragen over dit onderwerp hebben? Dan kan je <a href="mailto:abactis@ichthusutrecht.nl">abactis@ichthusutrecht.nl</a> mailen.
             </p>
         </div>
 
